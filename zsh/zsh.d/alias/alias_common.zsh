0cae1b78d83fc7061ee588e5d93942983c6f288d
rca <nem@nem.x0.to>
2024-10-06 08:59:04 +0900

    alias作成

---
+++ zsh/zsh.d/alias/alias_common.zsh
@@ -0,0 +1,12 @@
+# Prompt mode
+alias sonanoka="export rca_shell_prompt_mode=normal && source ~/.zshrc"
+alias nanoka="export rca_shell_prompt_mode=presentation && source ~/.zshrc"
+alias nyashi="export rca_shell_prompt_mode=nyashi && source ~/.zshrc"
+
+# Common
+alias cl="clear"
+alias lsa="ls -a"
+alias lsl="ls -l"
+alias lsla="ls -la"
+
+alias relogin="/bin/zsh --login"
---
+++ zsh/zsh.d/alias/alias_macos_arm.zsh
@@ -0,0 +1,2 @@
+alias arm="exec arch -arch arm64e /bin/zsh --login"
+alias x86="exec arch -arch x86_64 /bin/zsh --login"
